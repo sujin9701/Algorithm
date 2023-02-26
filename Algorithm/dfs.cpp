9e857afb9eaf54d892926dd506a4df35b0045227
Sujin Han <hansujin1998@naver.com>
2023-02-26 16:36:02 +0900

    [Basics] DFS 기본 코드

---
+++ Algorithm/dfs.cpp
@@ -0,0 +1,63 @@
+// DFS 기본 코드
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#define MAX 1001
+
+using namespace std;
+
+vector<int> v[MAX];
+bool visited[MAX];
+int n, m, s, a, b;
+
+void init() {
+    for (int i = 1; i <= n; i++) {
+        visited[i] = false;
+    }
+
+    return;
+}
+
+void dfs(int s) {
+  	stack<int> st;
+	st.push(s);
+
+	while (true) {
+		if (st.empty()) break;
+		int now = st.top();
+		st.pop();
+		if (visited[now]) continue;
+		visited[now] = true;
+		cout << now << " ";
+		for (int i = v[now].size() - 1; i >= 0; i--) {
+			st.push(v[now].at(i));
+		}
+	}
+
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m >> s;
+
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		// 양방향
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		sort(v[i].begin(), v[i].end());
+	}
+
+	init();
+	dfs(s);
+
+  	return 0;
+}
